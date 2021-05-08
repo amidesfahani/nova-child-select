7e5042e12e23cbe55436ae64b8cffbf6d5acbad0
Amid Esfahani <amid.esfahani@gmail.com>
2021-05-09 00:55:58 +0430

    Fix Tab Array

--- src/OptionsController.php
+++ src/OptionsController.php
@@ -3,6 +3,7 @@
 namespace Amid\ChildSelect;
 
 use Illuminate\Routing\Controller;
+use Laravel\Nova\Fields\FieldCollection;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class OptionsController extends Controller
@@ -15,8 +16,25 @@ class OptionsController extends Controller
         $resource = $request->newResource();
         $fields = $resource->updateFields($request);
         $field = $fields->findFieldByAttribute($attribute);
+
         if (!$field)
         {
+            if ($fields->count() > 1)
+            {
+                foreach ($fields as $_field)
+                {
+                    if (is_array($_field))
+                    {
+                        if (array_key_exists('panel', $_field) && $_field['panel'] == 'Tabs')
+                        {
+                            $_fields = FieldCollection::make($_field['fields']);
+                            $field = $_fields->findFieldByAttribute($attribute);
+                            break;
+                        }
+                    }
+                }
+            }
+            else
             if ($fields->count() == 1 && isset($fields['Tabs']))
             {
                 $tab = $fields->first();
